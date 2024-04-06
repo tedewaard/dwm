10543b51a7e0f1e829e5e9fe592b50da55863b1d
Trevor Edewaard <tr.edewaard@gmail.com>
2024-04-05 21:06:59 -0400

    Add catpuccin color scheme

--- config.def.h
+++ config.def.h
@@ -1,26 +1,28 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#1a1826";
+static const char col_gray2[]       = "#1e1e2e";
+static const char col_gray3[]       = "#302d41";
+static const char col_gray4[]       = "#575268";
+static const char col_mauve[]       = "#ddb6f2";
+static const char col_white[]       = "#d9e0ee";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_mauve, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray1, col_mauve, col_mauve },
 };
 
 static const char *const autostart[] = {
 	"slstatus", NULL,
     "compton", NULL,
+    "feh", "--bg-scale", "/home/tedewaard/Pictures/railroad.jpg", NULL,
     "flameshot", NULL,
 	NULL /* terminate */
 };
@@ -64,7 +66,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_mauve, "-sb", col_mauve, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "gnome-terminal", NULL };
 
 static const Key keys[] = {
